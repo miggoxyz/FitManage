fc7aa5074364972ecc0358503f84a80191ccda62
Ioakeim Mingo <kalokairi2019@protonmail.com>
2024-10-16 11:59:57 +0100

    Implement auth middleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+// Middleware to authenticate the JWT token
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token." });
+  }
+};
+
+// Middleware to check if the user is an admin
+const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admins only." });
+  }
+  next();
+};
+
+module.exports = { authenticateToken, isAdmin };
